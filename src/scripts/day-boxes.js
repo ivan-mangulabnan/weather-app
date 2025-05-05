974c4e69acf111f854a39bf223f8c7ad92192571
ivan <ivanmangulabnan19@gmail.com>
2025-05-05 22:32:39 +0800

    Create function for displaying daily weather report

---
+++ src/scripts/day-boxes.js
@@ -0,0 +1,39 @@
+import { DomEle } from "./dom.js";
+import { getGif } from "./get-gif.js";
+import { format } from "date-fns";
+
+export async function displayDailyWeather (weatherJSON) {
+  const outerDiv = document.createElement('div');
+  outerDiv.classList.add('weather-days');
+
+  const days = weatherJSON.days;
+  const dayDivs = await Promise.all(days.map(createDay));
+  dayDivs.forEach(div => outerDiv.appendChild(div));
+
+  DomEle.contentDiv.appendChild(outerDiv);
+}
+
+async function createDay (day) {
+  const wrapperDiv = document.createElement('div');
+  const date = document.createElement('p');
+  const img = document.createElement('img');
+  const description = document.createElement('p');
+  const temp = document.createElement('p');
+
+  date.textContent = format(day.datetime, 'MMMM d, yyyy');
+
+  try {
+    img.src = await getGif(day.icon);
+  } catch (err) {
+    console.log(`Error Happened: ${err}`);
+    img.alt = "Failed to load :(";
+  }
+
+  description.textContent = day.description;
+  temp.textContent = `${day.temp}°F / ${((parseFloat(day.temp) - 32) * 5/9).toFixed(1)}°C`;
+
+  wrapperDiv.classList.add('day-div');
+
+  wrapperDiv.append(date, img, description, temp);
+  return wrapperDiv;
+}
