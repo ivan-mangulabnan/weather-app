58ca3dbf08a482a0a3c1dabf5e830b87b10e6816
ivan <ivanmangulabnan19@gmail.com>
2025-05-06 20:08:43 +0800

    Add function in changing the hourly and adding styles to the selected day

--- src/index.js
+++ src/index.js
@@ -3,9 +3,20 @@ import { DomEle } from "./scripts/dom.js";
 import { getWeatherJSON } from "./scripts/get-weather.js";
 import { displayDailyWeather } from "./scripts/day-boxes.js";
 import { showSummary } from "./scripts/summary.js";
+import { hourlyDivs } from "./scripts/hour-boxes.js";
+import { cachedWeather } from "./scripts/get-weather.js";
 
 DomEle.searchBtn.addEventListener('click', e => {
   display();
+})
+
+DomEle.contentDiv.addEventListener('click', event => {
+  if (event.target.closest('.day-div')) {
+    const dayDiv = event.target.closest('.day-div');
+    const dayIndex = dayDiv.dataset.index;
+    changeHourly(dayIndex);
+    styleChosenDiv(event);
+  } 
 })
 
 async function display () {
@@ -13,4 +24,22 @@ async function display () {
   const weatherJSON = await getWeatherJSON();
   showSummary(weatherJSON);
   displayDailyWeather(weatherJSON);
+}
+
+async function changeHourly (index) {
+  let target = cachedWeather.days[index].hours;
+  let hourlyDiv = document.querySelector('.hour-div');
+
+  hourlyDiv.innerHTML = "";
+
+  const response = await Promise.all(target.map(hourlyDivs));
+  response.forEach(div => hourlyDiv.appendChild(div));
+}
+
+function styleChosenDiv (event) {
+  const dayDivs = document.querySelectorAll('.day-div');
+  Array.from(dayDivs).forEach(div => {
+    div.classList.remove('selected');
+    event.target.closest('.day-div').classList.add('selected');
+  })
 }
