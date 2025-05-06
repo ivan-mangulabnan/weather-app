4c5586f2eea2b4df3ada4b04f364f5a818931e41
ivan <ivanmangulabnan19@gmail.com>
2025-05-06 16:30:02 +0800

    Add cache for response weather API to dynamically show hourly per day

--- src/scripts/get-weather.js
+++ src/scripts/get-weather.js
@@ -1,10 +1,12 @@
 import { UserInput } from "./get-user-input.js";
 
+export let cachedWeather = null;
+
 export async function getWeatherJSON () {
   try {
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${UserInput.location}/2025-05-05/2025-05-06/?key=VZNB7QCW8AXR5TMH3UKK6ELY4`);
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${UserInput.location}?key=VZNB7QCW8AXR5TMH3UKK6ELY4`);
     const json = await response.json();
-    console.log(json);
+    cachedWeather = json;
     return json;
   } catch (err) {
     return err;
