24b98c3234ee5118c5923bc642d46bd6db3d826d
ivan <ivanmangulabnan19@gmail.com>
2025-05-07 14:24:03 +0800

    Change cache value from primitive  to an object for better conditional

--- src/scripts/get-weather.js
+++ src/scripts/get-weather.js
@@ -1,12 +1,16 @@
 import { UserInput } from "./get-user-input.js";
 
-export let cachedWeather = null;
+export let cachedWeather = {
+  location: null,
+  data: null
+};
 
 export async function getWeatherJSON () {
   try {
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${UserInput.location}?key=BTXJXKDWCKW43S3YA86JHUVU8`);
     const json = await response.json();
-    cachedWeather = json;
+    cachedWeather.location = UserInput.location;
+    cachedWeather.data = json;
     return json;
   } catch (err) {
     return err;
