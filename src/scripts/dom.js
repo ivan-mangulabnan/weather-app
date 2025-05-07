c71b9efe8fe4f6bb419b1532028356c96e1db3a5
ivan <ivanmangulabnan19@gmail.com>
2025-05-07 23:40:01 +0800

    Add dom elements in DomEle class

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -10,7 +10,19 @@ export class DomEle {
     return this.#cache.searchBtn ??= document.querySelector(`#searchBtn`);
   }
 
+  static get searchDiv () {
+    return this.#cache.searchDiv ??= document.querySelector(`#searchDiv`);
+  }
+
   static get contentDiv () {
     return this.#cache.contentDiv ??= document.querySelector(`#content`);
+  }
+
+  static get dateOne () {
+    return this.#cache.dateOne ??= document.querySelector(`#dateOne`);
+  }
+
+  static get dateTwo () {
+    return this.#cache.dateTwo ??= document.querySelector(`#dateTwo`);
   }
 }
